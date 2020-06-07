e922ba3bba2043414ee6c6bf0574d1204ce19557
Mohsen Esmailpour <mo.esmp@hotmail.com>
2020-06-07 10:50:15 +0430

    Initial commit.

---
+++ AdpSsmsSample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AdpSsmsSample
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var client = new AdpSmsService.JaxRpcMessagingServiceClient();
+            var response = await client.sendAsync("username", "password", "", new[] { "98999999999" }, null, null, new[] { "98999999999" }, 1, 2,
+                true, DateTime.Now, "This is a test messsage");
+
+            Console.WriteLine($"Result: {response.sendReturn.status} - {response.sendReturn.id}");
+            Console.Read();
+        }
+    }
+}
